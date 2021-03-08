21fe2f36288c69fde5e8faccf4b28f08c0982f3d
Bony Amin <jwelamin23@gmail.com>
2021-03-08 13:52:11 +0600

    Crud

--- src/components/pages/admin/Category.vue
+++ src/components/pages/admin/Category.vue
@@ -6,17 +6,48 @@
          <td>
            Category Name
          </td>
-         <td> <input type="text" id="newCatName" v-model="catname" placeholder="Category Name"></td>
+         <td> <input type="text" id="newCatName" v-model="newCat.name" placeholder="Category Name"></td>
        </tr>
         <tr>
          <td>
            Category Description
          </td>
-         <td><textarea v-model="catdes" type="text" placeholder="Category Description"></textarea> </td>       
+         <td><textarea v-model="newCat.description" type="text" placeholder="Category Description"></textarea> </td>       
        </tr>
        <tr>
          <td></td>
          <td><button class="btnSave" @click="addNewCategory()">Save</button></td>
+       </tr>
+     </table>
+    </modal>
+    <!-- Edit Modal -->
+     <modal modalHeading="Update Categry" :cond="showingEditModal" @modalClose="showingEditModal=false">
+     <table>
+       <tr>
+         <td>
+           Category Name
+         </td>
+         <td> <input type="text" id="newCatName" v-model="clickCat.name" placeholder="Category Name"></td>
+       </tr>
+        <tr>
+         <td>
+           Category Description
+         </td>
+         <td><textarea v-model="clickCat.description" type="text" placeholder="Category Description"></textarea> </td>       
+       </tr>
+       <tr>
+         <td></td>
+         <td><button class="btnSave" @click="updateCategory(clickCat.Id)">Update</button></td>
+       </tr>
+     </table>
+    </modal>
+<!-- DELETE -->
+    <modal modalHeading="Delete Categry" :cond="showingDeleteModal" @modalClose="showingDeleteModal=false">
+      <h2>You are going to delete the category '{{clickCat.name}}'</h2>
+     <table>    
+       <tr>
+         <td><button class="btnSave" @click="DeleteCat(clickCat.Id)">Yes</button></td>
+         <td><button class="btnClose" @click="showingDeleteModal = false">Close</button></td>
        </tr>
      </table>
     </modal>
@@ -32,12 +63,12 @@
             <th>Edit</th>
             <th>Delete</th>             
      </tr>
-     <tr v-for="(it,i) in newCatName">
-          <td>{{i+1}}</td>
+     <tr v-for="(it,i) in categoris">
+          <td>{{it.Id}}</td>
           <td>{{it.name}}</td>
           <td>{{it.description}}</td>
-          <td><button class="edit">Edit</button></td>
-          <td><button class="delete">Delete</button></td>
+          <td><button class="edit" @click="showingEditModal=true;clickCat=it;">Edit</button></td>
+          <td><button class="delete" @click="showingDeleteModal =true;clickCat=it">Delete</button></td>
      </tr>
    </table>
   </div>
@@ -49,17 +80,47 @@ export default {
   data () {
     return {
      showingAddModal:false,
-     catname:'',
-     catdes:'',
-     newCatName:[]
+    showingEditModal:false,
+    showingDeleteModal:false,
+    newCat:{
+      name:"",
+      description:""
+    },
+    clickCat:{},
+     categoris:[]
     }
-  },
+    },
   methods:{
-    addNewCategory(){
-      this.newCatName.push({name:this.catname,description:this.catdes});
-      this.showingAddModal=false,
-      console.log(this.newCatName);
+    addNewCategory:function(){
+     
+      this.categoris.push({Id:this.categoris.length + 1,name:this.newCat.name,description:this.newCat.description});
+      this.showingAddModal=false
+    },
+    updateCategory:function(id){
+        for(var i = 0; i < this.categoris.length; i++) {
+            if(this.categoris[i].Id === id) {
+               this.categoris[i].name=this.clickCat.name;
+                this.categoris[i].description=this.clickCat.description;
+                break;
+            }
+        }
+      
+       console.log(index);
+    // this.categoris[i].name=this.clickCat.name;
+    // this.categoris[i].description=this.clickCat.description;
+    this.showingEditModal = false
+    
+    },
+    DeleteCat:function(id){
+                for(var i = 0; i < this.categoris.length; i++) {
+                    if(this.categoris[i].Id === id) {
+                       this.categoris.splice(i,1);
+                        break;
+                    }
+                }
+                this.showingDeleteModal = false
     }
+   
     
   }
 }
